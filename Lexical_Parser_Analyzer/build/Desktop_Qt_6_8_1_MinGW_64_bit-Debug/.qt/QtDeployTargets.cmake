1d598cebc62e1de59a3c0e12c1d1af90a496a3f7
Roger <roger.a.salama@gmail.com>
2025-07-05 20:34:19 +0300

    Commit Lexical

---
+++ Lexical_Parser_Analyzer/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Lexical_Analyzer_FILE C:/Users/Lenovo/Desktop/AllahYenawarYaHandasa/Junior/Spring 25/Design of Compilers CSE439/Compilers Project/Phase2/Lexical_Analyzer/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Lexical_Analyzer.exe)
+set(__QT_DEPLOY_TARGET_Lexical_Analyzer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Lexical_Analyzer_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
---
+++ Lexical_Parser_Analyzer/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/deploy_Lexical_Analyzer_a47da97882.cmake
@@ -0,0 +1,8 @@
+include(C:/Users/Lenovo/Desktop/AllahYenawarYaHandasa/Junior/Spring 25/Design of Compilers CSE439/Compilers Project/Phase2/Lexical_Analyzer/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/Lexical_Analyzer-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE C:/Users/Lenovo/Desktop/AllahYenawarYaHandasa/Junior/Spring 25/Design of Compilers CSE439/Compilers Project/Phase2/Lexical_Analyzer/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Lexical_Analyzer.exe
+    GENERATE_QT_CONF
+)
